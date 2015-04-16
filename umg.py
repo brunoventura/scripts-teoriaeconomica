2100cb8b808dbb78e0e3617d879010d818d972e4
Bruno Ventura <brun0ventur4@gmail.com>
2015-04-16 17:01:41 -0300

    UMG utilizando matplotlib

--- umg.py
+++ umg.py
@@ -2,16 +2,21 @@
 # -*- coding: latin-1 -*-
 
 import sys
-from prettytable import PrettyTable
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
 
+del sys.argv[0]
 args = sys.argv
-table = PrettyTable(["Quantidade","UMG","Utilidade Total"])
+x = []
+y = []
+cells = []
 last = None
 total = 0
-for x in range(1, len(args)):
+for n in range(0, len(args)):
 	arg = None
 	try:
-		arg	= int(args[x])
+		arg	= int(args[n])
 	except ValueError:
 		print "Valor inválido informado"
 		break
@@ -22,6 +27,23 @@ for x in range(1, len(args)):
 		print "O valor não pode ser negativo"
 		break
 	total += arg
-	table.add_row([x, arg, total])
+	x.append(n+1)
+	y.append(arg)
+	cells.append([n+1, arg, total])
 	last = arg
-print table
+
+columns = ["Quantidade","UMG","Utilidade Total"]
+
+plt.subplots_adjust(top=0.8, left=0.2, right=0.8)
+
+plt.subplot(2, 1, 2)
+plt.plot(x, [a[2] for a in cells], '-o')
+plt.axis([0, len(args)*1.1, 0, total*1.1])
+
+plt.subplot(2, 1, 1)
+plt.plot(x, y, '-o')
+plt.axis([0, len(args)*1.1, 0, int(args[0])*1.1])
+
+plt.table(cellText = cells,cellLoc='center', colLabels=columns, loc='top')
+
+plt.show()
